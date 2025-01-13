6ac7486fb373540f195242be7c494f617c970aeb
Mamta Patil <mamtapatil44@gmail.com>
2025-01-13 15:40:04 -0500

    merged sorted array

---
+++ array/merge-sorted-array/index.js
@@ -0,0 +1,36 @@
+// merged tow sorted array without using fun sort
+
+function mergedSortedArray(arr1,arr2){
+    console.log("arr1 ",arr1)
+    console.log("arr2 ",arr2)
+    let mergedArr =[];
+    let i= 0;
+    let j =0;
+    while(i < arr1.length && j < arr2.length){
+    //   here we compare first elements of both array and whichever is small push it into merged array
+        if(arr1[i] < arr2[j] && arr1[i] !== mergedArr[i]){
+            mergedArr.push(arr1[i]);
+            i++;
+        } else if (arr1[i] > arr2[j] && arr2[j] !== mergedArr[j]){
+            mergedArr.push(arr2[j]);
+            j++;
+        }
+    }
+  console.log("mergedArr 111",mergedArr)
+    while(i< arr1.length ){
+        mergedArr.push(arr1[i]);
+        i++;
+    }
+    while(j <arr2.length ){
+        mergedArr.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArr;
+
+
+}
+
+console.log("mergedsorted array=== ",mergedSortedArray([1,3,5,7,9],[2,4,6,8]))
+
+console.log("mergedsorted array=== ",mergedSortedArray([1,3,5,7,9],[2,4,6,8,9]))
