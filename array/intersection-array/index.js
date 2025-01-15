0a5d069df275958e5c9def7aaf978195bfe1811b
Mamta Patil <mamtapatil44@gmail.com>
2025-01-15 12:40:53 -0500

    interscetions

--- array/intersection-array/index.js
+++ array/intersection-array/index.js
@@ -19,3 +19,30 @@ function findIntersectionItr(arr1, arr2) {
 const array1 = [1,2,3,4,5];
 const array2 = [3,4,5,6,7];
 console.log("intersection thr iteration === ",findIntersectionItr(array1,array2))
+console.log("====================================================")
+
+
+function findIntersectionFilter(arr1, arr2) {
+    let intersection = arr1.filter((ele)=>arr2.includes(ele));
+    return intersection;
+}
+
+console.log("intersection thr findIntersectionFilter === ",findIntersectionFilter(array1,array2))
+console.log("=======================================")
+
+
+
+
+
+function findIntersectionReduce(arr1, arr2) {
+   const inter= arr1.reduce((acc,current)=>{
+        if(arr2.includes(current)){
+            acc.push(current)
+        }
+        return acc
+    },[])
+  return inter;
+}
+
+console.log("intersection thr findIntersectionReduce === ",findIntersectionReduce(array1,array2))
+console.log("=======================================")
