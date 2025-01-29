05ca2df01e3f7782647f2b40e26867e463e36d2d
Mamta Patil <mamtapatil44@gmail.com>
2025-01-29 16:02:11 -0500

    password masking

---
+++ advance-string/password-masking/index.js
@@ -0,0 +1,10 @@
+// 4. Generating a Password Mask
+// Convert a given password into * of the same length.
+// 🔹 Hint: Convert each character into * using .repeat().
+ function passwordMasking(password){
+
+    return '*'.repeat(password.length);
+
+ }
+
+ console.log("paswword mask ",passwordMasking("Mamta"))
