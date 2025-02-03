cf2e755e74095600b6488d361b550690185da6f0
Mamta Patil <mamtapatil44@gmail.com>
2025-02-03 18:54:20 -0500

    session

---
+++ advance-array/two-pointer-technique/index.js
@@ -0,0 +1,12 @@
+// 9. Remove duplicate elements in-place from a sorted array.
+// oHint: Use two-pointer technique.
+// usecases og two pointers :
+// 1. merging sortted array
+// 2. find pairs with given sum
+// 3.reversing subarray
+// 4.checking plaindrom
+// 5.sliding window problems
+
+function removeDplicatesTwoPointerArray(arr){
+    // given array is sorted array
+}
---
+++ session-11/index.js
@@ -0,0 +1,6 @@
+const obj ={id:1,name:"mamta"}
+
+const map = new Map(Object.entries(obj));
+
+map.set("key",obj)
+console
